de96444379d945e7e4af90b0acbda6df3d2a08e9
Lucien <lucien.lopez@simpleandnew.com>
2022-07-23 18:09:17 +0200

    Added pending specs

---
+++ spec/services/poke_api_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PokeAPI do
+  describe '.resources_data' do
+    pending('TODO')
+  end
+
+  describe '.pokemons' do
+    pending('TODO')
+  end
+
+  describe '.types' do
+    pending('TODO')
+  end
+end
